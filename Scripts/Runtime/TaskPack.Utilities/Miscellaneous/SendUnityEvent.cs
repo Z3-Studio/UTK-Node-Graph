9b6701468d8bcd82a050895f2a7ec3e59973b66a
Master Z <andrefs.995@gmail.com>
2026-09-10 01:06:37 -0300

    Miscellaneous

---
+++ Scripts/Runtime/TaskPack.Utilities/Miscellaneous/SendUnityEvent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using Z3.NodeGraph.Core;
+using Z3.NodeGraph.Tasks;
+
+namespace Z3.NodeGraph.TaskPack.Utilities
+{
+    [NodeCategory(Categories.Miscellaneous)]
+    [NodeDescription("Send Unity Event")]
+    public class SendUnityEvent : ActionTask
+    {
+        [SerializeField] private Parameter<UnityEvent> unityEvent;
+
+        public override string Info => $"{unityEvent} Invoke";
+
+        protected override void UpdateAction()
+        {
+            unityEvent.Value.Invoke();
+            EndAction();
+        }
+    }
+}
